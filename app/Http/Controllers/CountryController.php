7122ff6b01a1e93447ff8147e94c9d99136d5f07
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-07-22 15:21:25 +0800

    ui views 分开

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -10,30 +10,44 @@ class CountryController extends Controller
     {
         return response()->json(['message' => "Hello $name"]);
     }
-
-
+    
     public function show(Request $request, $code = null)
+    {
+        if (strlen($code)>10){
+            return view('welcome', ['message' => "input too large"]);
+        }
+        $country = Country::where('code', $code)->first();
+        if (!$country) {
+            return view('welcome', ['message' => "Country not found"]);
+        }
+        return view('welcome', [
+            'country' => $country->en,
+            'code' => $country->code,
+            'tw' => $country->tw,
+            'locale' => $country->locale,
+            'zh' => $country->zh,
+            'lat' => $country->lat,
+            'lng' => $country->lng,
+            'emoji' => $country->emoji
+        ]);
+    }
+    public function showJson(Request $request, $code = null)
     {
         if (strlen($code)>10){
             return response()->json([
                 "success" => false,
                 "reason" => "Input is too long",  
-                view("welcome", ['message' => "Input is too long"])
-            ]);
-            
+            ]); 
         }
-        // Find the country by code
         $country = Country::where('code', $code)->first();
-
-        // Prepare response data
         if (!$country) {
             return response()->json([
+                
                 "success" => false,
-                "reason" => "Country not found",
-                view("welcome", ['message' => "Country not found"])
+                "reason" => "Country not found"
+                
             ]);
         }
-           
         return response()->json([
             "success" => true,
             'result'     => [
@@ -45,19 +59,7 @@ class CountryController extends Controller
                 'lat'     => $country->lat,
                 'lng'     => $country->lng,
                 'emoji'   => $country->emoji,
-             view('welcome', [
-                    'country' => $country->en,
-                    'code'    => $country->code,
-                    'tw'      => $country->tw,
-                    'locale'  => $country->locale,
-                    'zh'      => $country->zh,
-                    'lat'     => $country->lat,
-                    'lng'     => $country->lng,
-                    'emoji'   => $country->emoji,
-                ])
-        
     ]]);
-
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CountryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{code}', [CountryController::class, 'show']);
+Route::get('/json/{code}', [CountryController::class,'showJson']);
 Route::get('/greeting/{name}', [CountryController::class, 'greeting']);
 
 Route::get('/', function () {
