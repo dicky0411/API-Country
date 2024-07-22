43568f4fecb5d07c7b05490e2e000f1e849ee7d5
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-07-22 09:24:09 +0800

    api

--- store-app/app/Http/Controllers/CountryController.php
+++ store-app/app/Http/Controllers/CountryController.php
@@ -10,13 +10,22 @@ class CountryController extends Controller
     public function show($code)
     {
         $country = Country::where('code', $code)->first();
-
         if (!$country) {
             return response()->json([
                 'message' => 'Country not found'
             ], Response::HTTP_NOT_FOUND);
         }
 
-        return response()->json($country);
+        // Return view with country information
+        return view('country', [
+            'country' => $country->en,
+            'code' => $country->code,
+            'tw' => $country->tw,
+            'locale' => $country->locale,
+            'zh' => $country->zh,
+            'lat' => $country->lat,
+            'lng' => $country->lng,
+            'emoji' => $country->emoji
+        ]);
     }
 }
--- store-app/routes/web.php
+++ store-app/routes/web.php
@@ -3,11 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CountryController;
 
-Route::get('countries', [CountryController::class, 'index']);
-Route::get('countries/{id}', [CountryController::class, 'show']);
-Route::post('countries', [CountryController::class, 'store']);
-Route::put('countries/{id}', [CountryController::class, 'update']);
-Route::delete('countries/{id}', [CountryController::class, 'destroy']);
+Route::get('/{code}', [CountryController::class, 'show']);
 
 Route::get('/', function () {
     return view('welcome');
